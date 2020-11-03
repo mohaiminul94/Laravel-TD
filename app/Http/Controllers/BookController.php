3bc0f864921834f2534fc5c9379d99edf1aac85f
mohaiminul94 <rabidislam94@gmail.com>
2020-11-03 12:43:28 +0600

    Book test file updated

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+
+    protected function validateRequest()
+    {
+        return request()->validate([
+            'title'  => 'required',
+            'author'  => 'required',
+        ]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Book::create($this->validateRequest());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Book $book)
+    {
+        $book->update($this->validateRequest());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+
 
 class BookReservationTest extends TestCase
 {
@@ -13,10 +15,45 @@ class BookReservationTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+        $response= $this->post('book', [
+           'title'    => 'Cool Book Name',
+           'author'   => 'Victor'
+        ]);
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
     }
+
+    /** @test */
+    public function book_title_is_required()
+    {
+        $response= $this->post('book', [
+            'title'    => '',
+            'author'   => 'Victor'
+        ]);
+        $response->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('book', [
+            'title'    => 'Cool Book Name',
+            'author'   => 'Victor'
+        ]);
+        $book= Book::first();
+        $response= $this->put('book/'.$book->id, [
+            'title'    => 'New Title',
+            'author'   => 'New Author'
+        ]);
+        $this->assertEquals('New Title', Book::first()->title);
+        $this->assertEquals('New Author', Book::first()->author);
+    }
+
 }
